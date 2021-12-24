237d56325c92674246e2f622f4da9ad777b868e3
josedavid2021 <jmolinerdev@gmail.com>
2021-12-24 17:45:17 +0100

    últimos cambios

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        virensis: '#db373d'
+      },
+      spacing: {
+        '128': '32rem',
+        '256': '65rem',
+        '512': '110rem'
+      }
+    },
+  },
+  plugins: [],
+}
